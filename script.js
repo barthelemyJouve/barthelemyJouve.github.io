287098b0337f0a008d2e63e1441daef87654dd88
Barthélemy Jouve <jouveb@gmail.com>
2023-04-13 11:19:16 +0200

    animJsNavOk

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-const lien = document.getElementById("nav");
-const menu = document.getElementById("nav");
-console.log(menu);
+const nav = document.getElementById("nav");
+const linkNav1 = document.getElementById("navlink");
+const linkNav2 = document.getElementById("navlink1");
+const linkNav3 = document.getElementById("navlink2");
+const linkNav4 = document.getElementById("navlink3");
 
-lien.addEventListener("click", () => {
-  menu.classList.add("animnav");
+// questionContainer.addEventListener('événements', function)
+linkNav1.addEventListener("click", () => {
+  linkNav1.classList.toggle("classAnimClickNav");
+});
+linkNav1.addEventListener("click", () => {
+  nav.classList.toggle("classBackground");
+});
+
+linkNav2.addEventListener("click", () => {
+  linkNav2.classList.toggle("classAnimClickNav");
+});
+linkNav2.addEventListener("click", () => {
+  nav.classList.toggle("classBackground");
+});
+
+linkNav3.addEventListener("click", () => {
+  linkNav3.classList.toggle("classAnimClickNav");
+});
+linkNav3.addEventListener("click", () => {
+  nav.classList.toggle("classBackground");
+});
+
+linkNav4.addEventListener("click", () => {
+  linkNav4.classList.toggle("classAnimClickNav");
+});
+linkNav4.addEventListener("click", () => {
+  nav.classList.toggle("classBackground");
 });
