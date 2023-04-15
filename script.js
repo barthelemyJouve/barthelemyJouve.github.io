9547d3aa200eb0f436f175eedb3502812c4c6d0d
Barthélemy Jouve <jouveb@gmail.com>
2023-04-15 12:04:35 +0200

    menu

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const linkNav2 = document.getElementById("navlink1");
 const linkNav3 = document.getElementById("navlink2");
 const linkNav4 = document.getElementById("navlink3");
 
-// questionContainer.addEventListener('événements', function)
+// AnimMenuClick**********************
 linkNav1.addEventListener("click", () => {
   linkNav1.classList.toggle("classAnimClickNav");
 });
@@ -31,4 +31,32 @@ linkNav4.addEventListener("click", () => {
 });
 linkNav4.addEventListener("click", () => {
   nav.classList.toggle("classBackground");
+});
+
+// AnimMouseMoove
+const mousemove = document.querySelector(".mousemove");
+
+window.addEventListener("mousemove", (e) => {
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";
+});
+
+nav.addEventListener("mouseenter", () => {
+  nav.style.background = "rgba(4, 4, 4, 0.098)";
+});
+nav.addEventListener("mouseout", () => {
+  nav.style.background = "rgba(4, 4, 4, 0.098)";
+});
+nav.addEventListener("mouseover", () => {
+  nav.style.background = "rgb(4, 4, 4)";
+});
+
+// SCROLL NAV
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 60) {
+    nav.style.top = 0;
+  } else {
+    nav.style.top = "-120px";
+  }
 });
