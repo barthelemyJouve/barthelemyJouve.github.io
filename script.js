174669536f66236d4270df747ac2c23febaca85e
Barthélemy Jouve <jouveb@gmail.com>
2023-04-20 16:41:47 +0200

    MouseEffect2.0

--- script.js
+++ script.js
@@ -1,62 +1,26 @@
-const nav = document.getElementById("nav");
-const linkNav1 = document.getElementById("navlink");
-const linkNav2 = document.getElementById("navlink1");
-const linkNav3 = document.getElementById("navlink2");
-const linkNav4 = document.getElementById("navlink3");
-
-// AnimMenuClick**********************
-linkNav1.addEventListener("click", () => {
-  linkNav1.classList.toggle("classAnimClickNav");
-});
-linkNav1.addEventListener("click", () => {
-  nav.classList.toggle("classBackground");
-});
-
-linkNav2.addEventListener("click", () => {
-  linkNav2.classList.toggle("classAnimClickNav");
-});
-linkNav2.addEventListener("click", () => {
-  nav.classList.toggle("classBackground");
-});
-
-linkNav3.addEventListener("click", () => {
-  linkNav3.classList.toggle("classAnimClickNav");
-});
-linkNav3.addEventListener("click", () => {
-  nav.classList.toggle("classBackground");
-});
-
-linkNav4.addEventListener("click", () => {
-  linkNav4.classList.toggle("classAnimClickNav");
-});
-linkNav4.addEventListener("click", () => {
-  nav.classList.toggle("classBackground");
-});
-
-// AnimMouseMoove
-const mousemove = document.querySelector(".mousemove");
-
-window.addEventListener("mousemove", (e) => {
-  mousemove.style.left = e.pageX + "px";
-  mousemove.style.top = e.pageY + "px";
-});
-
-nav.addEventListener("mouseenter", () => {
-  nav.style.background = "rgba(4, 4, 4, 0.098)";
-});
-nav.addEventListener("mouseout", () => {
-  nav.style.background = "rgba(4, 4, 4, 0.098)";
-});
-nav.addEventListener("mouseover", () => {
-  nav.style.background = "rgb(4, 4, 4)";
-});
-
 // SCROLL NAV
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 60) {
+  if (window.scrollY > 10 && window.scrollY < 400) {
     nav.style.top = 0;
   } else {
     nav.style.top = "-120px";
   }
+});
+
+// MouseEffect;
+
+const mousemove = document.querySelector(".mousemove");
+const mousemove2 = document.querySelector(".mousemove2");
+const mousemove3 = document.querySelector(".mousemove3");
+
+window.addEventListener("mousemove", (e) => {
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";
+
+  mousemove2.style.left = e.pageX + "px";
+  mousemove2.style.top = e.pageY + "px";
+
+  mousemove3.style.left = e.pageX + "px";
+  mousemove3.style.top = e.pageY + "px";
 });
